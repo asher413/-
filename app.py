1a154153a8f69c31f527465b95b3150d3386aa7b
asher413 <a41337sh@gmail.com>
2026-03-05 19:34:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -190,5 +190,6 @@ def main_logic():
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    # הקוד הזה מוצא אוטומטית את הפורט שמתאים לשרת
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
